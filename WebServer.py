9baefb23bc06ad9907566a1d4525b9c909380c9c
GabiMinecraftDev <gabriel.m020913@gmail.com>
2026-08-19 11:19:45 +0200

    fix the webserver source file

--- WebServer.py
+++ WebServer.py
@@ -4,6 +4,7 @@ import vars
 
 app = Flask(__name__)
 
+app.register_blueprint(RoutesBP)
+
 if __name__ == "__main__":
-    app.register_blueprint(RoutesBP)
     app.run(host=vars.HOST, port=vars.PORT, debug=vars.DEBUG)
